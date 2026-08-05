f69a3e9f61dbebb5c066276870614ae63cecf20a
Fedor Stoliarov <fedor29062006@mail.ru>
2026-08-06 04:46:16 +0500

    ex2 completed

--- solution.go
+++ solution.go
@@ -1,5 +1,8 @@
+// Package ch10_solution contains the solution to the exercises in chapter 10 of Learning Go, 2nd edition.
 package ch10_solution
 
+// Add is a function that takes in two integer values and returns their sum.
+// It follows the rules specified by https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
